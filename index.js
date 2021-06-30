06a1112ace14fac1823c833c62125c3b08e76dca
Zach Hawkes <hawkeye.zach@gmail.com>
2021-06-30 17:19:34 -0600

    get a quick ping pong route going

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+
+const app = express();
+const port = 3000;
+
+app.get('/ping', pong);
+
+function pong(req, res) {
+  res.send('pong')
+}
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
+
+
